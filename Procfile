209cb49a3010d3c999b142b697dc2f25c0d3550c
Kyle <kcparks1234@gmail.com>
2023-04-28 14:56:47 -0700

    Updated server configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+web: concurrently "node server.js" "gunicorn app:app"
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 const ALPHA_VANTAGE_API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.NODE_PORT || 5000;
 
 const fetchStockPrice = async (symbol) => {
   const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${ALPHA_VANTAGE_API_KEY}`;
