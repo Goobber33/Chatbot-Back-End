0e09551418f88bbf9062792cb8094c038c2c77ee
Kyle <kcparks1234@gmail.com>
2023-04-28 14:06:13 -0700

    Added server.js file, and Alpha Advantage API to fetch stock prices for a given symbol

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const ALPHA_VANTAGE_API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
+const PORT = process.env.PORT || 5000;
+
+const fetchStockPrice = async (symbol) => {
+  const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${ALPHA_VANTAGE_API_KEY}`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    if (data['Error Message'] || data['Note']) {
+      return { error: 'Error fetching stock data' };
+    }
+
+    const stockInfo = data['Global Quote'];
+    return {
+      symbol: stockInfo['01. symbol'],
+      price: stockInfo['05. price'],
+    };
+  } catch (error) {
+    return { error: 'Error fetching stock data' };
+  }
+};
+
+app.get('/api/stock/:symbol', async (req, res) => {
+  const symbol = req.params.symbol;
+  const stockData = await fetchStockPrice(symbol);
+  res.json(stockData);
+});
+
+// Serve React app in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
