0bcc89144454792a9daf6cd826f7eb217b7987a6
Kyle <kcparks1234@gmail.com>
2023-04-28 15:28:51 -0700

    Added changes

--- server.js
+++ server.js
@@ -8,60 +8,50 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-
-// CORS configuration
-const corsOptions = {
-    origin: '*',
-    methods: 'GET,POST',
-    allowedHeaders: 'Content-Type,Authorization',
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 const ALPHA_VANTAGE_API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
 const PORT = process.env.NODE_PORT || 5000;
 
 const fetchStockPrice = async (symbol) => {
-    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${ALPHA_VANTAGE_API_KEY}`;
+  const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${ALPHA_VANTAGE_API_KEY}`;
 
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
 
-        if (data['Error Message'] || data['Note']) {
-            return { error: 'Error fetching stock data' };
-        }
-
-        const stockInfo = data['Global Quote'];
-        return {
-            symbol: stockInfo['01. symbol'],
-            price: stockInfo['05. price'],
-        };
-    } catch (error) {
-        return { error: 'Error fetching stock data' };
+    if (data['Error Message'] || data['Note']) {
+      return { error: 'Error fetching stock data' };
     }
+
+    const stockInfo = data['Global Quote'];
+    return {
+      symbol: stockInfo['01. symbol'],
+      price: stockInfo['05. price'],
+    };
+  } catch (error) {
+    return { error: 'Error fetching stock data' };
+  }
 };
 
 app.get('/api/stock/:symbol', async (req, res) => {
-    const symbol = req.params.symbol;
-    const stockData = await fetchStockPrice(symbol);
-    res.json(stockData);
+  const symbol = req.params.symbol;
+  const stockData = await fetchStockPrice(symbol);
+  res.json(stockData);
 });
 
 // Serve React app in production
 if (process.env.NODE_ENV === 'production') {
     // Serve static files from the React app
     app.use(express.static(path.join(__dirname, 'build')));
-
+  
     // Serve the index.html file for any request that doesn't match the API routes
-    app.get('*', (req, res, next) => {
-        if (req.path.startsWith('/api') || req.path.startsWith('/chatbot')) {
-            return next();
-        }
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    app.get(/^(?!\/(api|chatbot)).*$/, (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
-}
+  }
+  
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
